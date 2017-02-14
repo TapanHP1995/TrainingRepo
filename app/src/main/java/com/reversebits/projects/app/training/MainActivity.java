21b9d3160f89ac0f4b4ff33aa0ea603965b0d021
Vidhi <vidhi.bhut@inexture.in>
2017-02-14 18:42:05 +0530

    Adding form(changed in main xml)

--- app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
+++ app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
@@ -2,12 +2,39 @@ package com.reversebits.projects.app.training;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText preEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preEditText=(EditText)findViewById(R.id.preEditText);
+        preEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String prefix = "+91 ";
+
+                String currentString = editable.toString();
+
+                if (!currentString.startsWith(prefix)) {
+                    preEditText.setText(prefix);
+                    preEditText.setSelection(prefix.length());
+                }
+            }
+        });
     }
 }
