215e0dd93f7b8556a09b3f7fe9671fb73009dc34
priya5713 <priya.patel5713@gmail.com>
2017-02-15 18:44:57 +0530

    Custom Drawable

--- app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
+++ app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
@@ -10,20 +10,20 @@ import java.sql.BatchUpdateException;
 public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1=(Button)findViewById(R.id.button1);
-        button2=(Button)findViewById(R.id.button2);
-
+        final Button button1 = (Button)findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Button button1 = (Button)findViewById(R.id.button1);
+                Button button2 = (Button)findViewById(R.id.button2);
+
                 button1.setBackgroundResource(R.drawable.btn_selector);
+                button2.setBackgroundResource(R.drawable.btn_simple);
 
             }
         });
@@ -34,21 +34,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // TODO Auto-generated method stub
                 Button button2 = (Button)findViewById(R.id.button2);
+                Button button1 = (Button)findViewById(R.id.button1);
                 button2.setBackgroundResource(R.drawable.btn_selector);
+                button1.setBackgroundResource(R.drawable.btn_simple);
 
             }
         });
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
