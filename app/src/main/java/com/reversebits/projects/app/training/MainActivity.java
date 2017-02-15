59b04825810a81f758f6fdd081d148d5a2e2ec5c
Dhruvin Shah <dhruvin7@gmail.com>
2017-02-15 16:32:21 +0530

    Email layout

--- app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
+++ app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
@@ -3,9 +3,11 @@ package com.reversebits.projects.app.training;
 import android.app.LauncherActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -13,11 +15,35 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final EditText edi = (EditText) findViewById(R.id.edi);
+
+        edi.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    edi.setGravity(Gravity.LEFT);
+                    if(edi.getText().length()==0)
+                    {
+                        edi.setHint("");
+                    }
+                } else {
+                    edi.setHint("@gmail.com");
+
+                    if (edi.getText().length() > 0) {
+                        edi.setGravity(Gravity.LEFT);
+
+                        edi.setHint("");
+
+
+                    } else
+                        edi.setGravity(Gravity.RIGHT);
+                }
+            }
+        });
 
 
         spinner.setOnItemSelectedListener(this);
@@ -29,12 +55,14 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(dataAdapter);
-    }
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
-            String item = parent.getItemAtPosition(position).toString();
-    }
-        public void onNothingSelected(AdapterView<?> arg0){
 
-        }
+    }
 
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+
+    }
 }
