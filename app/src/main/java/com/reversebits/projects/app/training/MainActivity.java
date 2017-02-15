0f3ec36309fa2e041211420f794e868bbf4774d0
Dhruvin Shah <dhruvin7@gmail.com>
2017-02-15 12:54:32 +0530

    Email layout

--- app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
+++ app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
@@ -3,12 +3,38 @@ package com.reversebits.projects.app.training;
 import android.app.LauncherActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+        spinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("Male");
+        categories.add("Female");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
     }
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+            String item = parent.getItemAtPosition(position).toString();
+    }
+        public void onNothingSelected(AdapterView<?> arg0){
+
+        }
+
 }
