f7709e887f95b171bdb38ae3d69a794d59a68176
Dhruvin Shah <dhruvin7@gmail.com>
2017-02-17 18:29:23 +0530

    Layout tasks Complex done

---
+++ app/src/main/java/com/reversebits/projects/app/training/Fluid/Fluid.java
@@ -0,0 +1,8 @@
+package com.reversebits.projects.app.training.Fluid;
+
+/**
+ * Created by user on 17-02-2017.
+ */
+
+public class Fluid {
+}
---
+++ app/src/main/java/com/reversebits/projects/app/training/complex_layout/Complex2.java
@@ -0,0 +1,8 @@
+package com.reversebits.projects.app.training.complex_layout;
+
+/**
+ * Created by user on 17-02-2017.
+ */
+
+public class Complex2 {
+}
