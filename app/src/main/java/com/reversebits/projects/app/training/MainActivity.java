ba3ea90e8ad2d7d782d85992894b741fe8362cf2
priya5713 <priya.patel5713@gmail.com>
2017-02-22 14:53:12 +0530

    Animation

--- app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
+++ app/src/main/java/com/reversebits/projects/app/training/MainActivity.java
@@ -2,11 +2,10 @@ package com.reversebits.projects.app.training;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-
-import jp.wasabeef.glide.transformations.CropCircleTransformation;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +15,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_tf);
+        imgPic=(ImageView)findViewById(R.id.imgPic);
+
+
+        RotateAnimation anim = new RotateAnimation(0f, 350f, 15f, 15f);
+        // Step 2:  Set the Animation properties
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setDuration(700);
+
+        // Step 3: Start animating the image
+        imgPic.startAnimation(anim);
+
+        // Later. if you want to  stop the animation
+        // image.setAnimation(null);
 
 
 
@@ -43,12 +56,17 @@ public class MainActivity extends Activity {
 //        finish();
 //    }
 
-        final ImageView imageView=(ImageView)findViewById(R.id.imgPic);
+//        final ImageView imageView=(ImageView)findViewById(R.id.imgPic);
+//
+//
+//        Glide.with(this).load(R.drawable.download)
+//                .bitmapTransform(new CropCircleTransformation(this))
+//                .into((ImageView) findViewById(R.id.imgPic));
 
 
-        Glide.with(this).load(R.drawable.download)
-                .bitmapTransform(new CropCircleTransformation(this))
-                .into((ImageView) findViewById(R.id.imgPic));
+
+
+
 
 
 
